e523988831f0132e39f60d367714d3a319a6396a
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2021-04-02 14:30:04 +0530

    Increase session time

--- src/main/java/com/example/istocks/config/JwtTokenUtil.java
+++ src/main/java/com/example/istocks/config/JwtTokenUtil.java
@@ -17,7 +17,7 @@ import java.util.function.Function;
 public class JwtTokenUtil implements Serializable {
 
 
-    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 24; // *60 : minutes, *60 : hours, *24 : days
 
     @Value("${jwt.secret}")
     private String secret;
